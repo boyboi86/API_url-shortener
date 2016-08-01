52a53c057f21ade4534f5b88e490d007c5078c90
boyboi86 <boyboi86@gmail.com>
2016-08-01 07:27:57 +0000

    update db model

---
+++ url.model.js
@@ -0,0 +1,21 @@
+var shortid = require('shortid');
+
+var mongoose = require('mongoose'),
+     Schema = mongoose.Schema;
+
+/* defining a simple id within the shortUrl
+** mongodb will assign a custom _id to this new Schema
+** the users will use the simple id track any url instead
+*/
+
+var Url = new Schema({
+    originalUrl: {
+        type: String,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+        default: shortid.generate()
+    }
+})
